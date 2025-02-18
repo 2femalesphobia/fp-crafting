8baec5f360704e1db3a56e174f256c4e69deae30
2femalesphobia <151433124+2femalesphobia@users.noreply.github.com>
2025-02-18 18:17:53 +0000

    added config

--- config.lua
+++ config.lua
@@ -1,1 +1,39 @@
 Config ={}
+-- pistol 
+
+config.pistolsteel = '50'   -- amount of mats remooved once item is made 
+config.pistoliron = '50' -- amount of mats remooved once item is made 
+config.pistolmetalscrap = '50' -- amount of mats remooved once item is made 
+
+config.pistolammosteel = '5'   -- amount of mats remooved once item is made 
+config.pistolammoiron = '5'  -- amount of mats remooved once item is made 
+config.pistolammometalscrap = '5'   -- amount of mats remooved once item is made 
+
+
+--rifle 
+config.riflesteel = '50'   -- amount of mats remooved once item is made 
+config.rifleiron = '50' -- amount of mats remooved once item is made 
+config.riflemetalscrap = '50' -- amount of mats remooved once item is made 
+
+config.rifleammosteel = '5'   -- amount of mats remooved once item is made 
+config.rifleammoiron = '5'  -- amount of mats remooved once item is made 
+config.rifleammometalscrap = '5'   -- amount of mats remooved once item is made 
+
+--smg 
+config.smgsteel = '50'   -- amount of mats remooved once item is made 
+config.smgiron = '50' -- amount of mats remooved once item is made 
+config.smgmetalscrap = '50' -- amount of mats remooved once item is made 
+
+config.smgammosteel = '5'   -- amount of mats remooved once item is made 
+config.smgammoiron = '5'  -- amount of mats remooved once item is made 
+config.smgammometalscrap = '5'   -- amount of mats remooved once item is made
+
+
+--shotgun
+config.shotgunsteel = '50'   -- amount of mats remooved once item is made 
+config.shotguniron = '50' -- amount of mats remooved once item is made 
+config.shotgunmetalscrap = '50' -- amount of mats remooved once item is made 
+
+config.shotgunammosteel = '5'   -- amount of mats remooved once item is made 
+config.shotgunammoiron = '5'  -- amount of mats remooved once item is made 
+config.shotgunammometalscrap = '5'   -- amount of mats remooved once item is made
